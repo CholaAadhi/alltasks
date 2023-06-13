13de2e37a55fbf9d967ecd41b656045c9bc71862
CholaAadhi <136321684+CholaAadhi@users.noreply.github.com>
2023-06-13 13:46:44 +0530

    Add files via upload

---
+++ objectcreating.js
@@ -0,0 +1,11 @@
+var a = [{ CompanyName: "tomato" }
+    , { CompanyAddress:"northThj,thanjavur"}
+    , { CompanyCity:"thanjavur"}
+    , { Pincode:"123456"}
+    , { State:"tamilnadu"}
+    ,{branch:"thanjavur"}
+    ,{district:"thanjavur"}
+    ,{worker:"1000"}
+    ,{bus:"50"}
+    ,{car:"10"}];
+    console.log(a);
